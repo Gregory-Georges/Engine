c8b0b5a5de66dd18c2b42f0c0b14017570de51da
Gregory-Georges <simon.schetagne@gmail.com>
2023-05-17 14:05:02 -0400

    Added premake build lua file

---
+++ premake5.lua
@@ -0,0 +1,90 @@
+workspace "Engine"
+    architecture "x64"
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+project "Engine"
+    location "Engine"
+    kind "sharedlib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/**.h",
+            "%{prj.name}/**.hpp",
+            "%{prj.name}/**.cpp"
+        }
+
+        defines
+        {
+            "PLATFORM_LINUX",
+        }
+
+        postbuildcommands
+        {
+
+        }
+
+
+        filter "Configurations:Debug"
+            defines "ENGINE_DEBUG"
+            optimize "on"
+
+        filter "Configurations:Release"
+            defines "ENGINE_RELEASE"
+            optimize "on"
+
+        filter "Configurations:Dist"
+            defines "ENGINE_DIST"
+            optimize "on"
+
+
+
+project "Test"
+    location "Test"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/**.h",
+            "%{prj.name}/**.hpp",
+            "%{prj.name}/**.cpp"
+        }
+
+        defines
+        {
+            "PLATFORM_LINUX",
+        }
+
+        links
+        {
+            "Engine"
+        }
+
+
+        filter "Configurations:Debug"
+            defines "ENGINE_DEBUG"
+            optimize "on"
+
+        filter "Configurations:Release"
+            defines "ENGINE_RELEASE"
+            optimize "on"
+
+        filter "Configurations:Dist"
+            defines "ENGINE_DIST"
+            optimize "on"
