1455efe44545a76f339d78ca053cee824bdde171
Gregory-Georges <simon.schetagne@gmail.com>
2023-12-08 14:48:47 -0500

    Added build directory

---
+++ Build/Linux/Dependencies/glad/premake5.lua
@@ -0,0 +1,33 @@
+require "codeblocks"
+workspace "glad"
+    architecture "x64"
+    configurations
+    {
+        "Release"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    
+	project "glad"
+	    kind "StaticLib"
+	    language "C"
+	    staticruntime "off"
+	    
+	    targetdir (".")
+	    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	    files
+	    {
+		"include/glad/glad.h",
+		"include/KHR/khrplatform.h",
+		"src/glad.c"
+	    }
+
+	    includedirs
+	    {
+		"include"
+	    }
+
+	    filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
---
+++ Build/Linux/premake5.lua
@@ -0,0 +1,144 @@
+workspace "Engine"
+    architecture "x64"
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+
+------------------------------------------------------------------------------
+-- Engine
+------------------------------------------------------------------------------
+
+project "Engine"
+    location "Engine"
+    kind "staticlib"
+    language "C++"
+    cppdialect "c++17"
+    staticruntime "on"
+
+    targetdir ("Engine")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    pchheader "pch.hpp"
+    pchsource "pch.cpp"
+    
+    	includedirs
+    	{
+    	    ".",
+    	    "Dependencies/fmt/include",
+    	    "Dependencies/glad/include",
+    	    "Dependencies/glfw/include",
+    	    "Dependencies/glm/glm/",
+    	    "Dependencies/imgui",
+    	    "Dependencies/spdlog/include",
+    	    "Dependencies/stb"
+    	}
+
+        files
+        {
+            "%{prj.name}/**.h",
+            "%{prj.name}/**.c",
+            "%{prj.name}/**.hpp",
+            "%{prj.name}/**.cpp"
+        }
+
+        defines
+        {
+            "PLATFORM_LINUX",
+        }
+        
+        links
+        {
+            "Dependencies/fmt/fmt",
+            "Dependencies/glad/glad",
+            "Dependencies/glfw/src/glfw3",
+            "Dependencies/imgui/imgui",
+            "Dependencies/spdlog/spdlog",
+            "Dependencies/stb/stb"
+        }
+
+
+        filter "Configurations:Debug"
+            defines "ENGINE_DEBUG"
+            symbols "on"
+            runtime "debug"
+
+        filter "Configurations:Release"
+            defines "ENGINE_RELEASE"
+            optimize "on"
+            runtime "release"
+
+        filter "Configurations:Dist"
+            defines "ENGINE_DIST"
+            optimize "on"
+            runtime "release"
+
+
+
+------------------------------------------------------------------------------
+-- Test
+------------------------------------------------------------------------------
+
+project "Test"
+    location "Test"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "c++17"
+    staticruntime "on"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+        includedirs
+        {
+            "."
+        }
+    
+        files
+        {
+            "%{prj.name}/**.cpp",
+            "%{prj.name}/**.hpp",
+            "%{prj.name}/**.c",
+            "%{prj.name}/**.h",
+            "%{prj.name}/**.glsl"
+        }
+
+        defines
+        {
+            "PLATFORM_LINUX"
+        }
+
+        links
+        {
+            "Engine/Engine",
+            
+            -- Links from Engine
+            "Dependencies/fmt/fmt",
+            "Dependencies/glad/glad",
+            "Dependencies/glfw/src/glfw3",
+            "Dependencies/imgui/imgui",
+            "Dependencies/spdlog/spdlog",
+            "Dependencies/stb/stb"
+        }
+        
+        dependson
+        {
+            "Engine"
+        }
+
+
+        filter "Configurations:Debug"
+            symbols "on"
+
+        filter "Configurations:Release"
+            optimize "on"
+
+        filter "Configurations:Dist"
+            optimize "on"
