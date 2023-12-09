fafa909087f658131068fea4fe32cae9d8ab501d
Gregory-Georges <simon.schetagne@gmail.com>
2023-12-09 18:08:35 -0500

    Comments added to linux build script

--- Build/Linux/build.sh
+++ Build/Linux/build.sh
@@ -1,22 +1,28 @@
+# Copy build file and go to root
 cp ./premake5.lua ../../
 cd ../../
 
+# Build fmt
 cd ./Dependencies/fmt
 cmake .
 make
 
+# Build glad
 cd ../glad
 gcc -c src/glad.c -Iinclude/
 ar rvs libglad.a glad.o
 
+# Build glfw
 cd ../glfw
 cmake .
 make
 
+# Build glm
 cd ../glm
 cmake .
 make
 
+# Build dear imgui
 cd ../imgui
 cp backends/imgui_impl_opengl3_loader.h ./
 cp backends/imgui_impl_opengl3.h ./
@@ -26,13 +32,16 @@ cp backends/imgui_impl_glfw.cpp ./
 gcc -c *.cpp -Iinclude/
 ar rvs libimgui.a **.o
 
+# Build spdlog
 cd ../spdlog
 cmake .
 make
 
+# Build stb_image
 cd ../stb
 gcc -c stb_image.cpp -Iinclude/
 ar rvs libstb.a stb_image.o
 
+# Create main project file
 cd ../../
 premake5 codeblocks
